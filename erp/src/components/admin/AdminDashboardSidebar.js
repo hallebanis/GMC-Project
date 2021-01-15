25494d25d000d46f3c15b9e708130024982d6d00
Anis Halleb <halleb.anis6@gmail.com>
2021-01-15 11:37:37 +0100

    admin css

--- erp/src/components/admin/AdminDashboardSidebar.js
+++ erp/src/components/admin/AdminDashboardSidebar.js
@@ -5,27 +5,27 @@ import { Link } from "react-router-dom";
 const AdminDashboardSidebar = ({ color, linkList }) => {
   return (
     <Nav
-      style={{ backgroundColor: color }}
+      style={{ backgroundColor: color || "white" }}
       defaultActiveKey="/home"
       className="flex-column navSideBar"
     >
-      {linkList.map((elm, i) => (
-        <>
-          <Nav.Item className="navTitle" id={i}>
-            {elm.categorie}
-          </Nav.Item>
-          {elm.elements.map((sElm, j) => (
-            <Nav.Item>
-              <Nav.Link eventKey={`link-${j}`} id={`se${j}`}>
-                <Link to={sElm.link}>{sElm.title}</Link>
-              </Nav.Link>
+      {linkList &&
+        linkList.map((elm, i) => (
+          <>
+            <Nav.Item className="navTitle" id={i}>
+              {elm.categorie}
             </Nav.Item>
-          ))}
-        </>
-      ))}
+            {elm.elements.map((sElm, j) => (
+              <Nav.Item>
+                <Nav.Link eventKey={`link-${j}`} id={`se${j}`}>
+                  <Link to={sElm.link}>{sElm.title}</Link>
+                </Nav.Link>
+              </Nav.Item>
+            ))}
+          </>
+        ))}
     </Nav>
   );
 };
-
 
 export default AdminDashboardSidebar;
