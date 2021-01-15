80b3cbc48d2038f2728b69ed0c40fab83060d7a3
monia-souid <monia.souid@gmail.com>
2021-01-15 13:47:25 +0100

    added midelwares servers

--- back/server.js
+++ back/server.js
@@ -31,7 +31,18 @@ app.use("/api", require("./GRH/routes/pointage"));
 app.use("/api", require("./GRH/routes/service"));
 app.use("/api", require("./GRH/routes/prime"));
 
-
+// GA Routes
+app.use("/api", require("./GA/routes/categorieRoute"));
+app.use("/api", require("./GA/routes/chequeRoute"));
+app.use("/api", require("./GA/routes/commandeAchatRoute"));
+app.use("/api", require("./GA/routes/compteBancaireRoute"));
+app.use("/api", require("./GA/routes/factureRoute"));
+app.use("/api", require("./GA/routes/fournisseurRoute"));
+app.use("/api", require("./GA/routes/ligneAchatRoute"));
+app.use("/api", require("./GA/routes/ligneReservationRoute"));
+app.use("/api", require("./GA/routes/paiementRoute"));
+app.use("/api", require("./GA/routes/produitRoute"));
+app.use("/api", require("./GA/routes/reservationsRoute"));
 
 
 
