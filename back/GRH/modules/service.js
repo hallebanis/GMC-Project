a8cb465ad7f90ea20f7a2c9591734b8566ce9a01
HoussemJriri <jririhoussem07@gmail.com>
2021-01-16 14:18:11 +0100

    UPDATE

--- back/GRH/modules/service.js
+++ back/GRH/modules/service.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const service = new mongoose.Schema({
   nom: { type: String, required: true },
-  responsable: { type: String, required: true },
+  responsable: { type: mongoose.Types.ObjectId/* , required: true  */, ref:"personnel"},
 });
 
 module.exports = mongoose.model("service", service);
