a4c9e360e52fd4d89a0b38ee9ab73fa047daa770
sana-said <sana.said.aissa@gmail.com>
2021-01-21 14:08:05 +0100

    navbar

--- erp/src/pages/GV/Commande.js
+++ erp/src/pages/GV/Commande.js
@@ -1,14 +1,29 @@
 import React, { useEffect } from "react";
-import { Form } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
-import { getClient } from "../../actions/GV/venteActions";
+import { getClient, getCommandeVente } from "../../actions/GV/venteActions";
+import MainNavBar from "../../components/admin/MainNavBar";
+import GvSidebar from "../../components/GV/GvSidebar";
+import ListeCommandes from "../../components/GV/ListeCommandes"
 
 const Commande = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getClient());
+    dispatch(getCommandeVente())
   }, []);
-  return <Form></Form>;
+  const vente = useSelector(state => state.vente)
+  return (
+    <Container fluid>
+      <Row > <Col><MainNavBar /> </Col></Row>
+      <Row>
+        <Col md={3}>
+          <GvSidebar />
+        </Col>
+        <Col> <ListeCommandes ListeCommandes={vente.commandeVente} /> </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Commande;
