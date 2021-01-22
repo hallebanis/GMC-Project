6a03a7259737b062fa510978b6c60718ce11f860
HoussemJriri <jririhoussem07@gmail.com>
2021-01-22 12:05:21 +0100

    UPDATE

--- erp/src/pages/GRH/GrhDashboard.js
+++ erp/src/pages/GRH/GrhDashboard.js
@@ -6,18 +6,24 @@ import { ServiceForm } from "../../components/GRH/ServiceForm";
 import { Col, Container, Row } from "react-bootstrap";
 import AdminDashboardSidebar from "../../components/admin/AdminDashboardSidebar";
 import { NavSide } from "../../components/GRH/NavSide";
-import image from "../../res/img/system.png"
+import image from "../../res/img/system.png";
 import MainNavBar from "../../components/admin/MainNavBar";
 
 export const GrhDashboard = () => {
   return (
     <Container fluid>
-      <Row><Col><MainNavBar></MainNavBar></Col></Row>
+      <Row>
+        <Col>
+          <MainNavBar></MainNavBar>
+        </Col>
+      </Row>
       <Row height="80vh">
         <Col md={2}>
-          <NavSide/>
+          <NavSide />
         </Col>
-        <Col><img src={image}/></Col>
+        <Col>
+          <img src={image} />
+        </Col>
       </Row>
     </Container>
   );
