910953ec0ff2e3d2ade25f80e8ff8c29c57f5a56
Anis Halleb <halleb.anis6@gmail.com>
2021-01-22 12:15:51 +0100

    update produit schema

---
+++ erp/src/reducers/GA/achatReducer.js
@@ -0,0 +1,31 @@
+import {
+  ADD_COMMANDEVENTE_FAILED,
+  ADD_COMMANDEVENTE_SUCCESS,
+} from "../../actions/GV/types";
+
+const initState = {
+  facture: [],
+  commandeVente: [],
+  errors: null,
+};
+
+const achatReducer = (state = initState, action) => {
+  switch (action.type) {
+    case ADD_COMMANDEVENTE_SUCCESS:
+      return {
+        ...state,
+        commandeVente: [...state.commandeVente, action.payload],
+        errors: null,
+      };
+
+    case ADD_COMMANDEVENTE_FAILED:
+      return {
+        ...state,
+        errors: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default achatReducer;
