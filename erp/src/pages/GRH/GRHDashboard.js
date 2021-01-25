60ab235cb4237409391c08d31b94826d3f8c162c
Anis Halleb <halleb.anis6@gmail.com>
2021-01-25 13:36:16 +0100

    gestion fournisseur achat

--- erp/src/pages/GRH/GRHDashboard.js
+++ erp/src/pages/GRH/GRHDashboard.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const GRHDashboard = () => {
+import { Col, Container, Row } from "react-bootstrap";
+import { NavSide } from "../../components/GRH/NavSide";
+import image from "../../res/img/system.png";
+import MainNavBar from "../../components/admin/MainNavBar";
+import { useSelector } from "react-redux";
+
+export const GrhDashboard = ({ history }) => {
+  const auth = useSelector((state) => state.auth);
+  useEffect(() => {
+    if (!auth.isAuth) history.push("/login");
+  }, [auth, history]);
   return (
-    <div>
-      <h1>GRH Dashboard</h1>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col>
+          <MainNavBar color="#343A40"></MainNavBar>
+        </Col>
+      </Row>
+      <Row height="80vh">
+        <Col md={2}>
+          <NavSide />
+        </Col>
+        <Col>
+          <img src={image} alt="none" />
+        </Col>
+      </Row>
+    </Container>
   );
 };
-
-export default GRHDashboard;
+export default GrhDashboard;
